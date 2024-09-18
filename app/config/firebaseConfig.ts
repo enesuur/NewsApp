4d843b72379325f3d7469c44d9ba850677004ea3
Enes Uğur <71911663+enesuur@users.noreply.github.com>
2024-09-18 14:44:18 +0300

    Update firebaseConfig.ts

--- app/config/firebaseConfig.ts
+++ app/config/firebaseConfig.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCicpfTcGI8bPAKmB4AqLxvk4CNi0vC-NI",
-  authDomain: "myproject-4042a.firebaseapp.com",
-  projectId: "myproject-4042a",
-  storageBucket: "myproject-4042a.appspot.com",
-  messagingSenderId: "656606150699",
-  appId: "1:656606150699:web:e98c050e78ea6ca4e9aa6e"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
